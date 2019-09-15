4ffb1370ba05865baf1a6dcb8bae156858b25826
helenwxy <40188330+helenwxy@users.noreply.github.com>
2019-09-14 17:33:02 -0700

    hello darkness my old friend...

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { withRouter } from 'react-router-dom';
+import { tsConstructorType } from '@babel/types';
 
 function App() {
+  routeChange() {
+    let path = '';
+    this.prop.history.push(path);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="center-div">
+          <h3>
+            Are you a...
+          </h3>
+          <button className="App-buttons" onClick={this.routeChange}> I'm A Donor!
+          </button>
+          &nbsp;&nbsp;
+          <button className="App-buttons"> I'm a volunteer!</button>
+        </div>
+
     </div>
   );
 }
